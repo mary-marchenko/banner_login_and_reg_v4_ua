c68aacf3417983afa4e28f79a2e6b0369d708191
Mari <mari@MacBook-Pro.local>
2024-10-18 19:50:12 +0300

    mar-pad

--- front/js/main.js
+++ front/js/main.js
@@ -6,3 +6,14 @@
     setHeight();
     window.addEventListener('resize', setHeight);
 })();
+
+document.addEventListener("DOMContentLoaded", function() {
+    function isIOS() {
+        return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    }
+    let bannerTitleTop = document.querySelector('.banner__title-top');
+
+    if (!isIOS() && bannerTitleTop) {
+        bannerTitleTop.classList.add('mar-pad');
+    }
+});
